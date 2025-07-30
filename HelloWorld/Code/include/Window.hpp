9781619eb15f0a52a3f0439186d883395633f9ef
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-07-30 22:20:43 +0200

    [UPDATE] start writing some code to test things out

---
+++ HelloWorld/Code/include/Window.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+
+namespace HelloWorld
+{
+	class Window final : public QMainWindow
+	{
+
+	};
+}
--- HelloWorld/Code/src/main.cpp
+++ HelloWorld/Code/src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <QApplication>
 
-int main()
+#include "Window.hpp"
+
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello, World!\n";
+	QApplication app(argc, argv);
 
-	return 0;
+	QWidget window;
+	window.setFixedSize(800, 600);
+
+	QPushButton* button = new QPushButton("Hello World", &window);
+	button->setGeometry(10, 10, 80, 30);
+
+	window.show();
+	return app.exec();
 }
