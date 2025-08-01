376c9394b7a1ab3a48198252b9a4da4cc3f027e6
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-08-01 21:05:18 +0200

    [ADD] mini hello world code

--- HelloWorld/Code/src/Window.cpp
+++
@@ -1,6 +0,0 @@
-#include "Window.hpp"
-
-namespace HelloWorld
-{
-
-}
--- HelloWorld/Code/src/main.cpp
+++ HelloWorld/Code/src/main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <QApplication>
-
-#include "Window.hpp"
+#include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char* argv[])
 {
+	constexpr int windowWidth = 800;
+	constexpr int windowHeight = 600;
+	constexpr int buttonWidth = 80;
+	constexpr int buttonHeight = 60;
+	constexpr int buttonPosX = static_cast<int>(static_cast<float>(windowWidth) * 0.5f) - static_cast<int>(static_cast<float>(buttonWidth) * 0.5f);
+	constexpr int buttonPosY = static_cast<int>(static_cast<float>(windowHeight) * 0.5f) - static_cast<int>(static_cast<float>(buttonHeight) * 0.5f);
+
 	QApplication app(argc, argv);
 
 	QWidget window;
-	window.setFixedSize(800, 600);
+	window.setFixedSize(windowWidth, windowHeight);
 
-	QPushButton* button = new QPushButton("Hello World", &window);
-	button->setGeometry(10, 10, 80, 30);
+	QPushButton* button = new QPushButton("Click me!", &window);
+	button->setGeometry(buttonPosX, buttonPosY, buttonWidth, buttonHeight);
+
+	QObject::connect(button, &QPushButton::clicked, [button]()
+		{
+			button->setText("Hello, World!");
+		});
 
 	window.show();
 	return app.exec();
